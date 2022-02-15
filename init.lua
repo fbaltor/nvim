f6790d9979be51fc78dba3295e06cb9c128cfbac
Felipe Baltor <fbaltor@gmail.com>
2022-02-15 02:11:41 -0300

    [feat] Implemented diagnostics toggle button

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 
 require 'settings' -- lua/settings.lua
 require 'maps' -- lua/maps.lua
---require 'paq-config' -- lua/paq-config.lua
+require 'paq-config' -- lua/paq-config.lua
 require 'treesitter' -- lua/treesitter.lua
 require 'colors' -- lua/colorscheme.lua
 require 'lsp.lsp' -- lua/lsp.lua
--- lua/maps.lua
+++ lua/maps.lua
@@ -2,9 +2,22 @@
 
 local map = vim.api.nvim_set_keymap
 
----- map the leader key
---map('n', '<Space>', '', {})
---vim.g.mapleader = ' '
---
----- map ';' to ':' for easy command execution
---map('n', ';', ':', {noremap = true})
+-- map the leader key
+map('n', '<Space>', '', {})
+vim.g.mapleader = ' '
+
+-- map ';' to ':' for easy command execution
+map('n', ';', ':', {noremap = true})
+
+-- function to toggle diagnostics
+vim.g.diagnostics_visible = true
+function _G.toggle_diagnostics()
+	if vim.g.diagnostics_visible then 
+		vim.g.diagnostics_visible = false
+		vim.diagnostic.disable()
+	else
+		vim.g.diagnostics_visible = true
+		vim.diagnostic.enable()
+	end
+end
+map('n', '<leader>tt', ':call v:lua.toggle_diagnostics()<CR>', {noremap = true, silent = true})
